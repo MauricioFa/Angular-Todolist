44d9761dddcedbe75a07abea0bf511c40c2b215e
MauricioFa <mauriciositel@hotmail.com>
2020-02-16 19:07:49 -0500

    Solved routing. Login and register pages layout added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,16 @@ import { DemoComponent } from './pages/demo/demo.component';
 import { LoginComponent } from './pages/login/login.component';
 import { MainComponent } from './pages/main/main.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
 
 const routes: Routes = [
-  {path: '', component: DemoComponent},
-  {path: '/demo', component: DemoComponent},
-  {path: '/login', component: LoginComponent},
-  {path: '/main', component: MainComponent},
-  {path: '/register', component: RegisterComponent},
+  {path: '', component: LoginComponent},
+  {path: 'demo', component: DemoComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'main', component: MainComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: '**', component: NotfoundComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,15 @@ import { LoginComponent } from './pages/login/login.component';
 import { MainComponent } from './pages/main/main.component';
 import { HeaderComponent } from './components/header/header.component';
 import { DemoComponent } from './pages/demo/demo.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// imported from angular material
+import { MatCardModule } from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -16,11 +25,18 @@ import { DemoComponent } from './pages/demo/demo.component';
     LoginComponent,
     MainComponent,
     HeaderComponent,
-    DemoComponent
+    DemoComponent,
+    NotfoundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
